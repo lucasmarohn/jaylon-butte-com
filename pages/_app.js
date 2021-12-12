964d5d4a12862a91e57525b907d8f8eaa9e61bc5
lukemarohn <lukemarohn@gmail.com>
2021-12-11 21:45:18 -0800

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { theme } from '../theme'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={extendTheme(theme)}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
---
+++ theme.js
@@ -0,0 +1,28 @@
+export const theme = {
+    styles: {
+        global: {
+            "body": {
+                fontFamily: 'courier new',
+            }
+        }
+    },
+    components: {
+        Heading: {
+            baseStyle: {
+                fontFamily: 'courier new',
+                fontWeight: '600',
+            }
+        },
+        Container: {
+            baseStyle: {
+                px: 8,
+                maxW: 'container.lg'
+            }
+        }
+    },
+    colors: {
+        brand: {
+            900: "#000000",
+        }
+    }
+}

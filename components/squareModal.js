13d690efb5f81272df8b06fcafbe4ded5d1a6931
lukemarohn <lukemarohn@gmail.com>
2021-12-30 22:01:14 -0800

    add modal bg

--- components/squareModal.js
+++ components/squareModal.js
@@ -1,6 +1,7 @@
-import { Modal, ModalBody, ModalContent, ModalHeader, ModalCloseButton, Box } from "@chakra-ui/react"
+import { Modal, ModalBody, ModalContent, ModalHeader, ModalCloseButton, ModalOverlay, Box } from "@chakra-ui/react"
 const SquareModal = ({isOpen, onOpen, onClose}) => {
     return <Modal isOpen={isOpen} onOpen={onOpen} onClose={onClose} size="full" isCentered motionPreset="slideInBottom">
+        <ModalOverlay bg="white" opacity="1" />
         <ModalContent>
           <ModalHeader borderBottom="1px solid #e9e9e9" paddingBottom={8}>
             <ModalCloseButton />
